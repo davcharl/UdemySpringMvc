807f8bbfdf699e3797fa221fc965844cb11f2e03
unknown <davidc.2107@gmail.com>
2021-10-21 14:09:33 +0100

    comment out import

--- spring-boot/03-actuator-demo/src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController.java
+++ spring-boot/03-actuator-demo/src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -2,7 +2,7 @@ package com.luv2code.springboot.demo.mycoolapp.rest;
 
 import java.time.LocalDateTime;
 
-import org.springframework.beans.factory.annotation.Value;
+// import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
